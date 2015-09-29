e412d6538c322ba121c3858dd23770daaf6615bc
John Haugeland <stonecypher@gmail.com>
2015-09-29 07:07:00 -0700

    Working SCSS dumps for normal and email

--- config/args.js
+++ config/args.js
@@ -4,21 +4,22 @@
 module.exports = [
 
   { name: "input",       alias: "i", type: String,  defaultOption: true, multiple: true, description: "Name the [bold]{input} source file(s); applied in-order, last-wins" },
-  { name: "output",      alias: "o", type: String,                                       description: "[bold]{Output} base name (extension added; defaults last; omit or use hyphen for console out)" },
+  { name: "output",      alias: "o", type: String,                                       description: "(NO) [bold]{Output} base name (extension added; defaults last; omit or use hyphen for console out)" },
 
-  { name: "dir",         alias: "d", type: String,  defaultValue:  '.',                  description: "Set the [bold]{output directory} (defaults to '.')" },
+  { name: "dir",         alias: "d", type: String,  defaultValue:  '.',                  description: "(NO) Set the [bold]{output directory} (defaults to '.')" },
 
   { name: "help",        alias: "h",                                                     description: "Show this [bold]{help text}" },
 
-  { name: "explicit",    alias: "e", type: Boolean, defaultValue: false,                 description: "Spell out the properties not overridden (full override / [bold]{MDL reset})" },
+  { name: "explicit",    alias: "e", type: Boolean, defaultValue: false,                 description: "(NO) Spell out the properties not overridden (full override / [bold]{MDL reset})" },
 
-  { name: "verbose",     alias: "v", type: Boolean, defaultValue: false,                 description: "Tool should [bold]{dump diagnostics} while working" },
-  { name: "silent",      alias: "s", type: Boolean, defaultValue: false,                 description: "Tool should [bold]{emit nothing} but error codes" },
+  { name: "verbose",     alias: "v", type: Boolean, defaultValue: false,                 description: "(NO) Tool should [bold]{dump diagnostics} while working" },
+  { name: "silent",      alias: "s", type: Boolean, defaultValue: false,                 description: "(NO) Tool should [bold]{emit nothing} but error codes" },
 
   { name: "mdl",         alias: "1", type: String,  defaultValue: true,                  description: "Output an [bold]{mdl.css} transform" },
-  { name: "css",         alias: "2", type: String,  defaultValue: true,                  description: "Output a compiled [bold]{css} from the SASS" },
-  { name: "email",       alias: "3", type: String,  defaultValue: true,                  description: "Output a lightweight email-appropriate [bold]{css}" },
-  { name: "material-ui", alias: "4", type: String,  defaultValue: true,                  description: "Output a [bold]{material-ui} transform" },
-  { name: "ng-material", alias: "5", type: String,  defaultValue: true,                  description: "Output a [bold]{ng-material} transform" }
+  { name: "css",         alias: "2", type: String,  defaultValue: true,                  description: "(NO) Output a compiled [bold]{css} from the SASS" },
+  { name: "email",       alias: "3", type: String,  defaultValue: true,                  description: "Output a lightweight email-appropriate [bold]{scss}" },
+  { name: "emailcss",    alias: "4", type: String,  defaultValue: true,                  description: "(NO) Output email-appropriate compiled [bold]{css}" },
+  { name: "material-ui", alias: "5", type: String,  defaultValue: true,                  description: "(NO) Output a [bold]{material-ui} transform" },
+  { name: "ng-material", alias: "6", type: String,  defaultValue: true,                  description: "(NO) Output a [bold]{ng-material} transform" }
 
 ];
